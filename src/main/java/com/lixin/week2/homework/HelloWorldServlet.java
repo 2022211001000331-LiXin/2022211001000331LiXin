32d0e97ea798ee768f98a940a0e2a1110d9ba271
2022211001000331LiXin <2145143087@qq.com>
2024-03-10 22:01:44 +0800

    Second Commit

---
+++ src/main/java/com/lixin/week2/homework/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package com.lixin.week2.homework;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,HttpServletResponse response)throws IOException {
+        PrintWriter writer=response.getWriter();
+        writer.println("Name:Li Xin");
+        writer.println("ID:2022211001000331");
+        writer.println("Date and Time:"+new Date());
+    }
+    public void doPost(HttpServletRequest request,HttpServletResponse response){
+
+    }
+}
